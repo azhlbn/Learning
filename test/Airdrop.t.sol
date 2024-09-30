b6508d5538bcd0b7e64f605ba464c5c7442e62a8
aleksey <azhlbn@gmail.com>
2024-09-30 16:29:27 +0300

    init

---
+++ test/Airdrop.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+
+
+contract CounterTest is Test {
+
+
+    function setUp() public {
+
+    }
+}
